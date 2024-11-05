842227a69f178cdf09d78989b211cf4e913f4506
Gulnara <adinadzulia@gmail.com>
2024-11-05 09:01:36 +0500

    home5

---
+++ 04.11.2024/falsetrue.js
@@ -0,0 +1,21 @@
+/*
+//1tap
+let age =70
+let isStudent = false
+if(age>=65) || isStudent===true
+{
+    alert("Сізге жеңілдік бар")
+}
+else{
+    alert("Сізге жеңілдік жоқ")
+}
+*/
+
+//2TAP
+let num=Number(prompt("сан енгіз"))
+if (10<num&& num<=20){
+    alert(true)
+}
+else{
+    alert(false)
+}
--- 04.11.2024/homew.js
+++ 04.11.2024/homew.js
@@ -1,42 +1,42 @@
-let a=10, b=10; с=12;
-let isStudent=true;
-let isAlumni=true;//выпускник немесе түлек
-let san=0;//false
+let a = 10, b = 10; с = 12;
+let isStudent = true;
+let isAlumni = true;//выпускник немесе түлек
+let san = 0;//false
 
-console.log("San:",!san); // San:true
+console.log("San:", !san); // San:true
 
-if(a=""){
+if (a = "") {
     console.log("Бос жол");
 }
-else{
+else {
     console.log("a-да мән бар");
 }
 
 console.log(!isStudent);
 // ==(тең)
-console.log(a==b);
+console.log(a == b);
 
 // console.log(a==b);//true
 //== (тең емес)
-console.log(a!=b); //false
+console.log(a != b); //false
 
 //>
-console.log(a>b); //false
+console.log(a > b); //false
 
 //<
-console.log(a<b); //false
+console.log(a < b); //false
 
 //>(үлкен немесе тең)
-console.log(a>=b); // 9>=10false
+console.log(a >= b); // 9>=10false
 
 //>=(кіші немесе тең)
-console.log(a<=b); // 9<=10true
+console.log(a <= b); // 9<=10true
 
 //&&(және) екі шарттың екеуі де true болған жағдайда,true қайтарады
-console.log((a>b)) && ((c>a)); // 9>10(false) 12>9(true) =>false
+console.log((a > b)) && ((c > a)); // 9>10(false) 12>9(true) =>false
 
 //||(немесе) екі шарттың кемінде біреуі ғана true болған жағдайда,true қайтарады
-console.log((a>b)) || ((c>a)); // 9>10(false) 12>9(true) =>true
+console.log((a > b)) || ((c > a)); // 9>10(false) 12>9(true) =>true
 
 
 
@@ -48,12 +48,12 @@ console.log((a>b)) || ((c>a)); // 9>10(false) 12>9(true) =>true
 
 
 //Күрделі өрнек 
-let age=20, hasTicket=true,isVip=false;
+let age = 20, hasTicket = true, isVip = false;
 
-if((age>=18&&hasTicket ) || isVip){
+if ((age >= 18 && hasTicket) || isVip) {
     console.log("Сізге кіруге болады!");
 }
-else{
+else {
     console.log("Сізге кіруге тыйым салынады!");
 }
 
