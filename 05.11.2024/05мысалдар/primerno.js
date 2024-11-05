1a032e04dd8c7483a042a807b1dc33d11ec4b0d6
Gulnara <adinadzulia@gmail.com>
2024-11-05 12:16:26 +0500

    go

--- 05.11.2024/05мысалдар/primerno.js
+++ 05.11.2024/05мысалдар/primerno.js
@@ -4,5 +4,5 @@ let num= prompt("Сан енгізіңіз")
 if(num % 2 ==0){
     alert("Сіздің жазған саныңыз" +num + "жұп сан")
 }else{
-    alert("Сіздің жазған саныңыз" +num + "тақ сан")uiiiiiiiiiiiiiiiiiiiiiiiuiuuuuuuuuuuuuuuuuuuuujhhb
+    alert("Сіздің жазған саныңыз" +num + "тақ сан")
 } 
